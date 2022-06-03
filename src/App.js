a44d259176048ea8caa16ca3c1ae10a9c4942509
Whycodex <neerajgupta12300@gmail.com>
2022-06-03 18:16:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { Component } from 'react';
+
+import CardList from './components/card-list/card-list/card-list.component';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+  
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response)=>response.json())
+    .then((users)=>this.setState(()=>{
+      return {monsters:users}
+    }
+    ));
+  }
+
+  onSearchChange=(event)=>{
+    const searchField=event.target.value.toLocaleLowerCase();
+    this.setState(()=>{
+      return {searchField};
+    });
+  }
+  
+  render() {
+    
+    const {monsters, searchField}=this.state;
+    const {onSearchChange}=this;
+
+    const filteredMonsters=monsters.filter((monster)=>{
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    return (
+      <div className="App">
+        <input
+         className='search-box'
+         type='search'
+         placeholder='search monsters'
+         onChange={onSearchChange}
+        />
+        
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    );
+  }
+}
+
+export default App;
