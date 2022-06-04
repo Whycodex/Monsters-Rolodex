024374f18663239b531de3a64faa567017167c87
Whycodex <neerajgupta12300@gmail.com>
2022-06-04 16:58:05 +0530

    finished project 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 
 import CardList from './components/card-list/card-list/card-list.component';
+import SearchBox from './components/card-list/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -39,13 +40,12 @@ class App extends Component {
     });
     return (
       <div className="App">
-        <input
-         className='search-box'
-         type='search'
-         placeholder='search monsters'
-         onChange={onSearchChange}
+        <h1 className='app-title'>Monsters Rolodex</h1>
+        <SearchBox
+          className='monsters-search-box'
+          onChangeHandler={onSearchChange}
+          placeholder='search monsters' 
         />
-        
         <CardList monsters={filteredMonsters}/>
       </div>
     );
